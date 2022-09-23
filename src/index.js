a7ca215b7472a642b4c60fb4c19a5051caf5b6a9
David Burgess <david@aline.digital.co.uk>
2022-09-23 13:39:44 +0100

    finished implementing theme and run build script

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import store from './app/store'
 import { Provider } from 'react-redux'
 
 
-console.log('Initial state: ', store.getState())
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
